e0ec17153dccb4b8d77424cab050badb30695c33
starduststr <starduststr0@gmail.com>
2024-06-30 19:18:37 +0700

    feat: add requerements

--- test.py
+++ test.py
@@ -1,25 +1,4 @@
-import requests
-import json
+from app.api.v1.GetDetailNewsByGenAI import GetDetailNewsByGenAI
 
-url = "https://sxzh8vmg-4005.asse.devtunnels.ms/news/updateWithUrlRequest"
 
-payload = json.dumps({
-  "urlRequestId": "4bbb537b-a717-47da-9aef-c8865174de8d",
-  "title": "Ganti News Title",
-  "description": "Ganti Deskripsi",
-  "author": "Dzikri Faza",
-  "source": "Narasi",
-  "publishDate": "2023-11-28 19:30:43.934129",
-  "newsKeywords": "Mengajukan, Klimis",
-  "isTraining": True,
-  "trainingDate": "2023-11-28 19:30:43.934129",
-  "label": "NOT TRAINED",
-  "location": "Bandung"
-})
-headers = {
-  'Content-Type': 'application/json',
-  }
-
-response = requests.request("POST", url, headers=headers, data=payload)
-
-print(response.text)
+GetDetailNewsByGenAI.scrape_dynamic("https://sulbartv.com/2024/05/01/smsi-dan-kedubes-iran-sepakat-jalin-kerja-sama/", "dihuhe7")
